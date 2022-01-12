e6c9118c297b32281166a9829e5289dab7d62f9c
Yana Drozd <yana_drozd@epam.com>
2022-01-12 11:37:37 +0200

    [EDP-DDM-11119] Update dependencies approach
    Change-Id: I8bd5482af53356731ba55f29b29e57f4e22712f9

--- src/test/java/tests/SearchConditionsStepDefinitionsTest.java
+++ src/test/java/tests/SearchConditionsStepDefinitionsTest.java
@@ -1,4 +1,4 @@
-package platform.qa.tests;
+package tests;
 
 import org.junit.jupiter.api.Test;
 
@@ -6,6 +6,5 @@ public class SearchConditionsStepDefinitionsTest {
 
     @Test
     void user_execute_Api_with_Path_and_Parameters() {
-
     }
 }
