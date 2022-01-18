78d308ad257253247d2a9c0a4db1ad3708606349
Yana Drozd <yana_drozd@epam.com>
2022-01-18 13:35:23 +0200

    [EDP-DDM-11119] Add logger
    Change-Id: Ia6666ba80ef4db4263baeb9a62af7b6ea311894f

--- src/main/java/platform/qa/base/FileUtils.java
+++ src/main/java/platform/qa/base/FileUtils.java
@@ -5,11 +5,16 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import org.apache.commons.io.FilenameUtils;
+import org.apache.log4j.Logger;
 
 public class FileUtils {
+    private static final Logger LOG = Logger.getLogger(FileUtils.class);
+
     public static String readFromFile(String path, String name) {
         try {
-            return Files.readString(Path.of(path, FilenameUtils.getName(name)), StandardCharsets.UTF_8);
+            Path pathToFile = Path.of(path, FilenameUtils.getName(name));
+            LOG.info("Read from file: " + pathToFile);
+            return Files.readString(pathToFile, StandardCharsets.UTF_8);
         } catch (IOException e) {
             throw new RuntimeException("File was not found!: ", e);
         }
--- src/main/java/platform/qa/steps/DatabaseStepDefinitions.java
+++ src/main/java/platform/qa/steps/DatabaseStepDefinitions.java
@@ -11,7 +11,10 @@ import platform.qa.cucumber.TestContext;
 import platform.qa.db.TableInfoDb;
 import platform.qa.enums.Context;
 
+import org.apache.log4j.Logger;
+
 public class DatabaseStepDefinitions extends BaseSteps {
+    private static final Logger LOG = Logger.getLogger(DatabaseStepDefinitions.class);
     private TestContext testContext;
 
     public DatabaseStepDefinitions(TestContext testContext) {
@@ -28,12 +31,14 @@ public class DatabaseStepDefinitions extends BaseSteps {
     @Тоді("виконується запит до бази даних з файлу {string}")
     public void executeQueryFromFile(@NonNull String selectFileName) {
         String selectQuery = FileUtils.readFromFile("src/test/resources/data/queries/", selectFileName);
+        LOG.info("Execute query: " + selectQuery);
         var result = new TableInfoDb(getPrimarySourceDb()).waitAndGetEntity(selectQuery);
         testContext.getScenarioContext().setContext(Context.DB_RESULT_LIST, result);
     }
 
     @Тоді("виконується запит до бази даних:")
     public void executeQuery(@NonNull String selectQuery) {
+        LOG.info("Execute query: " + selectQuery);
         var result = new TableInfoDb(getPrimarySourceDb()).waitAndGetEntity(selectQuery);
         testContext.getScenarioContext().setContext(Context.DB_RESULT_LIST, result);
     }
