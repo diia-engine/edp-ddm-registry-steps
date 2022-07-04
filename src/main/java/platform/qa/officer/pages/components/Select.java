07b6d7328b8a7b93aceb788d8657629635866a42
Vadym Smyrnov <vadym_smyrnov@epam.com>
2022-07-04 13:04:08 +0300

    [EDP-DDM-13091] Back to the original Select
    Change-Id: Ib3537e8607884558bf78690825b3bb4b75af8e9c

--- src/main/java/platform/qa/officer/pages/components/Select.java
+++ src/main/java/platform/qa/officer/pages/components/Select.java
@@ -1,21 +1,27 @@
 package platform.qa.officer.pages.components;
 
-import lombok.SneakyThrows;
-import org.openqa.selenium.WebElement;
-import platform.qa.base.BasePage;
-
 import static java.lang.String.format;
 import static org.openqa.selenium.By.xpath;
 import static org.openqa.selenium.Keys.ARROW_DOWN;
 import static org.openqa.selenium.Keys.ENTER;
+import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfAllElements;
+
+import lombok.SneakyThrows;
+import platform.qa.base.BasePage;
+
+import java.util.List;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class Select extends BasePage {
 
-    private final String selectInputPath = "//label[text()[contains(.,\"%s\")]]" +
-            "/following-sibling::div//input[@type='text']";
+    private final String selectDropdownPath = "//label[text()[contains(.,'%s')"
+            + "]]/following-sibling::div//input[@type='text']";
 
-    private final String selectDropdownPath = "//label[text()[contains(.,\"%s\")]]" +
-            "/parent::div//div[@role='combobox']";
+    @FindBy(xpath = "//ul[contains(@class,'MuiAutocomplete-listbox')]/li")
+    private List<WebElement> selectItems;
 
     public Select() {
         loadingPage();
@@ -24,20 +30,18 @@ public class Select extends BasePage {
 
     @SneakyThrows
     public void selectItemFromDropDown(String itemName, String itemValue) {
-        String selectXPath = getSelectInputXPath(itemName);
-        WebElement selectInput = driver.findElement(xpath(selectXPath));
-        selectInput.click();
-        Thread.sleep(1000); // needs to be replaced in the future
-        selectInput.sendKeys(itemValue);
-        Thread.sleep(1000); // needs to be replaced in the future
-        selectInput.sendKeys(ARROW_DOWN, ENTER);
+        String selectXPath = getSelectXPath(itemName);
+        WebElement select = driver.findElement(xpath(selectXPath));
+        wait.until(ExpectedConditions.elementToBeClickable(select))
+                .click();
+        select.sendKeys(itemValue);
+        wait.until(visibilityOfAllElements(selectItems));
+        wait.until((ExpectedCondition<Boolean>) driver -> selectItems.stream().noneMatch(item -> item.getText().isEmpty()));
+        select.sendKeys(ARROW_DOWN, ENTER);
+        wait.until((ExpectedCondition<Boolean>) driver -> !select.getAttribute("value").isEmpty());
     }
 
-    private String getSelectInputXPath(String itemName) {
-        return format(selectInputPath, itemName);
-    }
-
-    private String getSelectDropdownXPath(String itemName) {
+    private String getSelectXPath(String itemName) {
         return format(selectDropdownPath, itemName);
     }
 }
